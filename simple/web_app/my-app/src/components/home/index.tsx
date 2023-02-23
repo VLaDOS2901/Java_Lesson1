c639df27888cae5385ab4a0baa37b909094fa61d
Vlad Revutskyi <vlad.rewutsky01@gmail.com>
2023-02-24 00:46:57 +0200

    Task 11

--- simple/web_app/my-app/src/components/home/index.tsx
+++ simple/web_app/my-app/src/components/home/index.tsx
@@ -1,4 +1,6 @@
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import {ICategory} from "./types";
 /*
   This example requires some changes to your config:
   
@@ -14,36 +16,49 @@
   ```
 */
 const callouts = [
-    {
-      name: 'Desk and Office',
-      description: 'Work from home accessories',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
-      imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
-      href: '#',
-    },
-    {
-      name: 'Self-Improvement',
-      description: 'Journals and note-taking',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
-      imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
-      href: '#',
-    },
-    {
-      name: 'Travel',
-      description: 'Daily commute essentials',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
-      imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
-      href: '#',
-    },
-  ]
+  {
+    name: 'Desk and Office',
+    description: 'Work from home accessories',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
+    imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
+    href: '#',
+  },
+  {
+    name: 'Self-Improvement',
+    description: 'Journals and note-taking',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
+    imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
+    href: '#',
+  },
+  {
+    name: 'Travel',
+    description: 'Daily commute essentials',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
+    imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+    href: '#',
+  },
+]
+
+const Home = () => {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8084/api/categories").then((res) => {
+      setCategories(res.data);
+    });
+  }, []);
   
-  const Home = () => {
-    return (
+  return (
+    <>
+    <h2 className="text-2xl font-bold text-gray-900 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">Категорії</h2>
+    <ul className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 text-2xl">
+    {categories.map((c:any)=>
+      <li key={c.id}>#{c.id}  {c.name}</li>)}
+      </ul>
       <div className="bg-gray-100">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
             <h2 className="text-2xl font-bold text-gray-900">Collections</h2>
-  
+
             <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
               {callouts.map((callout) => (
                 <div key={callout.name} className="group relative">
@@ -67,7 +82,8 @@ const callouts = [
           </div>
         </div>
       </div>
-    )
-  }
-  
-  export default Home;
+    </>
+  )
+}
+
+export default Home;
